8ac5df92220c2b1bc02fd29ac236f0a26836630d
worldofdeveloper <developer.brest@gmail.com>
2021-07-27 00:24:41 +0300

    add search by name

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const navigation = document.querySelector("#navigation")
 const main = document.querySelector("#main")
 const modal = document.querySelector("#exampleModal")
+const search = document.querySelector('#search')
+const inputSearch = document.querySelector('#inputSearch')
 
 let requestURL = 'https://swapi.dev/api/people/?page=1'
 
@@ -9,13 +11,11 @@ let peoples = [];
 let pages = 0;
 let pageSelected = 1;
 
-document.addEventListener('click', function(event) {
-
-  console.log(peoples)
-   for (let i=0; i<peoples.length; i++){
-     if (event.path[0].id === `btn-${i}`)
+document.addEventListener('click', function (event) {
+  for (let i = 0; i < peoples.length; i++) {
+    if (event.path[0].id === `btn-${i}`)
       modal.innerHTML =
-      `
+        `
       <div class="modal-dialog">
               <div class="modal-content">
                 <div class="modal-header">
@@ -35,60 +35,68 @@ document.addEventListener('click', function(event) {
               </div>
             </div>
       `
-   }
-
+  }
 });
 
 navigation.onclick = function (event) {
-    currentSelected = event.path[1].innerText;
-    if (currentSelected === "Previous")
-        pageSelected--;
-    else if (currentSelected === "Next")
-        pageSelected++;
-    else pageSelected = currentSelected;
-    console.log(pageSelected)
-    requestURL = `https://swapi.dev/api/people/?page=${pageSelected}`
-    updatePage()
+  currentSelected = event.path[1].innerText;
+  console.log(currentSelected)
+  if (currentSelected === "Previous") {
+    pageSelected--
+    if (pageSelected < 1)
+      pageSelected = pages
+  }
+  else if (currentSelected === "Next") {
+    pageSelected++
+    if (pageSelected > pages)
+      pageSelected = 1;
+  }
+  else pageSelected = currentSelected;
+  console.log(pageSelected)
+  requestURL = `https://swapi.dev/api/people/?page=${pageSelected}`
+  updatePage()
+}
+
+
+search.onclick = function (event) {
+  requestURL = `https://swapi.dev/api/people/?search=${inputSearch.value}`
+  updatePage()
 }
 
 function sendRequest(method, url) {
-    return fetch(url).then(response => response.json())
+  return fetch(url).then(response => response.json())
 }
 
 updatePage()
 
 function updatePage() {
-    sendRequest('GET', requestURL)
-        .then(data => {
-            peoples = data.results
-            count = +data.count
-            pagination()
-            cards()
-        })
-        .catch(err => console.log(err))
+  sendRequest('GET', requestURL)
+    .then(data => {
+      peoples = data.results
+      count = +data.count
+      cards()
+      pagination()
+    })
+    .catch(err => console.log(err))
 }
 
 function pagination() {
-    navigation.innerHTML = ""
-    pages = Math.ceil(count / 10)
-    navigation.innerHTML += `<li class="page-item"><a class="page-link" href="#" tabindex="-1" aria-disabled="true" id="minus">Previous</a></li>`
-    for (let i = 1; i < pages + 1; i++)
-        navigation.innerHTML += `<li class="page-item" id="page-${(i)}"><a class="page-link" href="#">${(i)}</a></li>`
-    navigation.innerHTML += `<li class="page-item"><a class="page-link" href="#" tabindex="+1" aria-disabled="true" id="plus">Next</a></li>`
-    let liSet = document.querySelector(`#page-${pageSelected}`)
-    liSet.classList.add('active')
-    if (pageSelected === 1)
-        navigation.firstChild.classList.add('disabled')
-    else if (pageSelected === pages)
-        navigation.lastChild.classList.add('disabled')
+  navigation.innerHTML = ""
+  pages = Math.ceil(count / 10)
+  navigation.innerHTML += `<li class="page-item"><a class="page-link" href="#" tabindex="-1" aria-disabled="true" id="minus">Previous</a></li>`
+  for (let i = 1; i < pages + 1; i++)
+    navigation.innerHTML += `<li class="page-item" id="page-${(i)}"><a class="page-link" href="#">${(i)}</a></li>`
+  navigation.innerHTML += `<li class="page-item"><a class="page-link" href="#" tabindex="+1" aria-disabled="true" id="plus">Next</a></li>`
+  let liSet = document.querySelector(`#page-${pageSelected}`)
+  liSet.classList.add('active')
 }
 
 function cards() {
-    main.innerHTML = ""
-    for (let i = 0; i < peoples.length; i++) {
-        if (peoples[i].gender === "male") {
-            main.innerHTML +=
-                `<li class="col page-item">
+  main.innerHTML = ""
+  for (let i = 0; i < peoples.length; i++) {
+    if (peoples[i].gender === "male") {
+      main.innerHTML +=
+        `<li class="col page-item">
             <div class="card h-100">
               <img src="https://for-male.ru/wp-content/uploads/2020/11/stilnyj-muzhchina.jpg" class="card-img-top" alt="male">
               <div class="card-body">
@@ -98,10 +106,10 @@ function cards() {
               </div>
             </div>
           </li>`
-        } else
-            if (peoples[i].gender === "female") {
-                main.innerHTML +=
-                    `<li class="col page-item">
+    } else
+      if (peoples[i].gender === "female") {
+        main.innerHTML +=
+          `<li class="col page-item">
             <div class="card h-100">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRG4vSyA2zSAm16XDEF3i8SDAZw6tHQUY2ixA&usqp=CAU" class="card-img-top" alt="female">
               <div class="card-body">
@@ -111,9 +119,9 @@ function cards() {
               </div>
             </div>
           </li>`
-            } else {
-                main.innerHTML +=
-                    `<li class="col page-item">
+      } else {
+        main.innerHTML +=
+          `<li class="col page-item">
             <div class="card h-100">
             <img src="https://im.kommersant.ru/Issues.photo/CORP/2019/07/23/KMO_111307_25942_1_t222_135708.jpg" class="card-img-top" alt="robot">
               <div class="card-body">
@@ -123,8 +131,8 @@ function cards() {
               </div>
             </div>
           </li>`
-            }
-    }
+      }
+  }
 }
 
 
